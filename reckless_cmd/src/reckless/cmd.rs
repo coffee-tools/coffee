90027179e0b71235968665a6618474d203c9263b
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-09-13 18:44:04 +0100

    cmd: add sub command line defintion
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- reckless_cmd/src/reckless/cmd.rs
+++ reckless_cmd/src/reckless/cmd.rs
@@ -1,6 +1,8 @@
-//! Reckless command line arguments!
+//! Reckless command line arguments definition.
 use clap::{Parser, Subcommand};
 
+
+/// Reckless main command line definition for the command line tools.
 #[derive(Debug, Parser)]
 #[clap(name = "rkl")]
 #[clap(about = "A reckless plugin manager for core lightning", long_about = None)]
@@ -11,8 +13,19 @@ pub struct RecklessArgs {
     conf: Option<String>,
 }
 
+/// Reckless subcommand of the command line daemon.
 #[derive(Debug, Subcommand)]
 enum RecklessCommand {
+    /// Install a single or a list of plugins.
     #[clap(arg_required_else_help = true)]
     Install,
+    /// upgrade a single or a list of plugins.
+    #[clap(arg_required_else_help = true)]
+    Upgrade,
+    /// Print the list of plugin installed in cln.
+    #[clap(arg_required_else_help = true)]
+    List,
+    /// Remove a plugin installed in cln.
+    #[clap(arg_required_else_help = true)]
+    Remove,
 }
