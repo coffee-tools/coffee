17795569208a47f46d7c6881169945e0fb20ee0a
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2023-04-04 13:58:42 +0200

    fea(lib): support the sh macros and add support for poetry
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- coffee_lib/src/macros.rs
+++ coffee_lib/src/macros.rs
@@ -7,4 +7,35 @@ macro_rules! error {
     }};
 }
 
+#[macro_export]
+macro_rules! sh {
+    ($root: expr, $script:expr, $verbose:expr) => {
+        let script = $script.trim();
+        let cmds = script.split("\n"); // Check if the script contains `\`
+        debug!("cmds: {:#?}", cmds);
+        for cmd in cmds {
+            debug!("cmd {:#?}", cmd);
+            let cmd_tok: Vec<&str> = cmd.split(" ").collect();
+            let command = cmd_tok.first().unwrap().to_string();
+            let mut cmd = Command::new(command);
+            cmd.args(&cmd_tok[1..cmd_tok.len()]);
+            cmd.current_dir($root);
+            if $verbose {
+                let _ = cmd
+                    .spawn()
+                    .expect("Unable to run the command")
+                    .wait()
+                    .await?;
+            } else {
+                let _ = cmd.output().await?;
+            }
+        }
+    };
+
+    ($root:expr, $script:expr) => {
+        sh!($root, $script, false)
+    };
+}
+
 pub use error;
+pub use sh;
