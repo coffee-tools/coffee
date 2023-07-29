b130003833e2e260e402bc1698177f5d30be6be7
Tarek <tareknaser360@gmail.com>
2023-07-29 22:20:19 +0300

    fix: use error! macro
    Signed-off-by: Tarek <tareknaser360@gmail.com>

--- coffee_github/src/utils.rs
+++ coffee_github/src/utils.rs
@@ -1,4 +1,5 @@
 use coffee_lib::errors::CoffeeError;
+use coffee_lib::macros::error;
 use coffee_lib::url::URL;
 use log::debug;
 
@@ -17,31 +18,31 @@ pub async fn clone_recursive_fix(repo: git2::Repository, url: &URL) -> Result<()
                 debug!("at path {}", &path);
                 Ok(())
             }
-            Err(err) => Err(CoffeeError::new(1, err.message())),
+            Err(err) => Err(error!("{}", err.message())),
         }?;
     }
     Ok(())
 }
 
 pub fn fast_forward(path: &str, branch: &str) -> Result<UpgradeStatus, CoffeeError> {
-    let repo = git2::Repository::open(path).map_err(|err| CoffeeError::new(1, err.message()))?;
+    let repo = git2::Repository::open(path).map_err(|err| error!("{}", err.message()))?;
 
     repo.find_remote("origin")
-        .map_err(|err| CoffeeError::new(1, err.message()))?
+        .map_err(|err| error!("{}", err.message()))?
         .fetch(&[branch], None, None)
-        .map_err(|err| CoffeeError::new(1, err.message()))?;
+        .map_err(|err| error!("{}", err.message()))?;
 
     let fetch_head = repo
         .find_reference("FETCH_HEAD")
-        .map_err(|err| CoffeeError::new(1, err.message()))?;
+        .map_err(|err| error!("{}", err.message()))?;
 
     let fetch_commit = repo
         .reference_to_annotated_commit(&fetch_head)
-        .map_err(|err| CoffeeError::new(1, err.message()))?;
+        .map_err(|err| error!("{}", err.message()))?;
 
     let analysis = repo
         .merge_analysis(&[&fetch_commit])
-        .map_err(|err| CoffeeError::new(1, err.message()))?;
+        .map_err(|err| error!("{}", err.message()))?;
 
     if analysis.0.is_up_to_date() {
         Ok(UpgradeStatus::UpToDate)
@@ -49,23 +50,20 @@ pub fn fast_forward(path: &str, branch: &str) -> Result<UpgradeStatus, CoffeeErr
         let refname = format!("refs/heads/{}", branch);
         let mut reference = repo
             .find_reference(&refname)
-            .map_err(|err| CoffeeError::new(1, err.message()))?;
+            .map_err(|err| error!("{}", err.message()))?;
 
         reference
             .set_target(fetch_commit.id(), "Fast-Forward")
-            .map_err(|err| CoffeeError::new(1, err.message()))?;
+            .map_err(|err| error!("{}", err.message()))?;
 
         repo.set_head(&refname)
-            .map_err(|err| CoffeeError::new(1, err.message()))?;
+            .map_err(|err| error!("{}", err.message()))?;
 
         match repo.checkout_head(Some(git2::build::CheckoutBuilder::default().force())) {
             Ok(()) => return Ok(UpgradeStatus::Updated),
-            Err(err) => return Err(CoffeeError::new(1, err.message())),
+            Err(err) => return Err(error!("{}", err.message())),
         }
     } else {
-        Err(CoffeeError::new(
-            1,
-            "Error trying to pull the latest changes",
-        ))
+        Err(error!("{}", "Error trying to pull the latest changes",))
     }
 }
