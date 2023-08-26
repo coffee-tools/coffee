35f56c90bf5596f3e899775d7562e92332a3b836
jacksn <jkranawetter05@gmail.com>
2023-08-26 15:55:13 +0200

    fix(lib): simplify and improve sh! macro
    instead of trying to manually parse the script and potentially making errors,
    let the actual `sh` command do the work correctly

--- coffee_lib/src/macros.rs
+++ coffee_lib/src/macros.rs
@@ -15,31 +15,27 @@ macro_rules! error {
 macro_rules! sh {
     ($root: expr, $script:expr, $verbose:expr) => {{
         let script = $script.trim();
-        let cmds = script.split("\n"); // Check if the script contains `\`
-        log::debug!("cmds: {:?}", cmds);
-        for cmd in cmds {
-            log::debug!("cmd {:?}", cmd);
-            let cmd_tok: Vec<&str> = cmd.split(" ").collect();
-            let command = cmd_tok.first().unwrap().to_string();
-            let mut cmd = Command::new(command);
-            cmd.args(&cmd_tok[1..cmd_tok.len()]);
-            cmd.current_dir($root);
-            let command = if $verbose {
-                cmd.spawn()
-                    .expect("Unable to run the command")
-                    .wait_with_output()
-                    .await?
-            } else {
-                cmd.output().await?
-            };
+        log::debug!("script: {:?}", script);
 
-            if !command.status.success() {
-                let mut content = String::from_utf8(command.stderr).unwrap();
-                if content.trim().is_empty() {
-                    content = String::from_utf8(command.stdout).unwrap();
-                }
-                return Err(CoffeeError::new(2, &content));
+        let mut cmd = Command::new("sh");
+        cmd.args(&["-c", &script]);
+        cmd.current_dir($root);
+
+        let command = if $verbose {
+            cmd.spawn()
+                .map_err(|_| error!("Unable to run the command"))?
+                .wait_with_output()
+                .await?
+        } else {
+            cmd.output().await?
+        };
+
+        if !command.status.success() {
+            let mut content = String::from_utf8(command.stderr).unwrap();
+            if content.trim().is_empty() {
+                content = String::from_utf8(command.stdout).unwrap();
             }
+            return Err(CoffeeError::new(2, &content));
         }
     }};
 
