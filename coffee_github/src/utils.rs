63ca832b1b8963e0da5c20449ecb5cf16157412e
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2024-02-26 22:33:46 +0100

    git: adding basic function to checkout a branch
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- coffee_github/src/utils.rs
+++ coffee_github/src/utils.rs
@@ -1,8 +1,10 @@
+use log::debug;
+use tokio::process::Command;
+
 use coffee_lib::errors::CoffeeError;
 use coffee_lib::macros::error;
 use coffee_lib::url::URL;
 use coffee_lib::{commit_id, get_repo_info, sh};
-use log::debug;
 
 use coffee_lib::types::response::UpgradeStatus;
 
@@ -30,14 +32,34 @@ pub async fn git_upgrade(
     branch: &str,
     verbose: bool,
 ) -> Result<UpgradeStatus, CoffeeError> {
-    use tokio::process::Command;
-
     let repo = git2::Repository::open(path).map_err(|err| error!("{}", err.message()))?;
 
     let (local_commit, _) = get_repo_info!(repo);
 
     let mut cmd = format!("git fetch origin\n");
     cmd += &format!("git reset --hard origin/{branch}");
+    sh!(path, cmd, verbose);
+
+    let (upstream_commit, date) = get_repo_info!(repo);
+
+    if local_commit == upstream_commit {
+        Ok(UpgradeStatus::UpToDate(upstream_commit, date))
+    } else {
+        Ok(UpgradeStatus::Updated(upstream_commit, date))
+    }
+}
+
+pub async fn git_checkout(
+    path: &str,
+    branch: &str,
+    verbose: bool,
+) -> Result<UpgradeStatus, CoffeeError> {
+    let repo = git2::Repository::open(path).map_err(|err| error!("{}", err.message()))?;
+    let (local_commit, _) = get_repo_info!(repo);
+
+    let mut cmd = format!("git fetch origin\n");
+    cmd += &format!("git reset --hard\n");
+    cmd += &format!("git checkout origin/{branch}");
     sh!(path, cmd, verbose);
 
     let (upstream_commit, date) = get_repo_info!(repo);
